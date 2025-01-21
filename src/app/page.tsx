b3598f92d74626fef287143692f31efd165bb736
Arif Wahyu <arifwahyu696@gmail.com>
2025-01-21 23:36:22 +0700

    add splash screen page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
-export default function Home() {
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function SplashScreenPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace("/auth/register");
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
-    <div className="p-10">
-      <h1 className="text-4xl">Ini menggunakan font Poppins secara default</h1>
-      <p className="text-lg mt-4 font-roboto">Ini menggunakan font Roboto</p>
+    <div className="w-full min-h-dvh bg-black flex justify-center items-center">
+      <p className="text-4xl md:text-xl text-greenPrimary font-bold">srawung</p>
     </div>
   );
 }
