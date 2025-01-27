a6f72fc8acd4fcd2852c9a8946399d5955e1bdee
Arif Wahyu <arifwahyu696@gmail.com>
2025-01-27 22:53:40 +0700

    dynamic img

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -2,13 +2,27 @@
 
 import useScreenWidth from "@/libs/useScreenWidth";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 interface Props {
   children: React.ReactNode;
 }
 
 export default function AuthLayout({ children }: Props) {
+  const [imgLanding, setImgLanding] = useState<string>(
+    "/images/social-friends.webp"
+  );
+  const pathname = usePathname();
   const sw = useScreenWidth();
+
+  useEffect(() => {
+    if (pathname === "/auth/password") {
+      setImgLanding("/images/lock.webp");
+    } else if (pathname === "/auth/login") {
+      setImgLanding("/images/hands.webp");
+    }
+  }, [pathname]);
 
   return (
     <div className="w-full h-full min-h-dvh bg-black flex flex-col md:flex-row">
@@ -18,7 +32,7 @@ export default function AuthLayout({ children }: Props) {
             <Image
               fill
               className="object-cover"
-              src="/images/social-friends.webp"
+              src={imgLanding}
               alt=""
               loading="lazy"
             />
